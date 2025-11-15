90a2617518ede109b84dbcd3de044b674572ec61
Mina Samir <mina15g4y@gmail.com>
2025-11-15 11:21:38 +0200

    Fix: Set correct base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/blue-ecommerce-main/",
-  plugins: [react()]
+  base: "/my-vite-portfolio/", // <== هذا هو التعديل الصحيح
+  plugins: [react()]
 });
